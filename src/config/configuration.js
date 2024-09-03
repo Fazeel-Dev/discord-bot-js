994d31bb41102de71eb540796dcc7383bedc8d86
Muhamed Fazeel <fazeel642@gmail.com>
2024-09-03 17:48:31 +0530

    chore: update configuration

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -8,6 +8,20 @@ const config = {
 	discord: {
 		guildId: env.GUILD_ID,
 	},
+	colors: {
+		primary: '#ff002b',
+	},
+	embeds: {
+		error: {
+			color: '#ff002b',
+			title: 'Error',
+			description: 'An error has occurred. Please try again later.',
+		},
+		success: {
+			color: '#00ff00',
+			title: 'Success',
+		},
+	},
 };
 
 module.exports = config;
